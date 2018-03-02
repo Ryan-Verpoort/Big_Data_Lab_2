be2c48658818f3bf552310c061f655af664ee1c0
Unknown <95ry95ry@gmail.com>
2018-03-02 13:42:29 +0200

    Ignore a.out and sections openmp

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "omp.h"
 
 int main()
 {
-    clock_t begin = clock();
+
 
 
     int** A;
-    int N = 100;
-    int ConstN = 2;
+    int N = 4;
     A = malloc(N * sizeof(int*)); // pointer to pointer array
 
     // allocate the memory for the entire array
@@ -17,13 +17,14 @@ int main()
     for(int k = 0; k < N; k++) {
 	A[k] = malloc(N * sizeof(int));
     }
-
+	
+	clock_t begin1 = clock();
     for(int j = 0; j < N; j++) { // Fill up all the indices of A
 	for(int i = 0; i < N; i++) {
-	    A[j][i] = (ConstN * i) + j;
+	    A[j][i] = (N * i) + j;
 	}
     }
-
+	clock_t end1 = clock();
     for(int j = 0; j < N; j++) { // Prints the original array
 	for(int i = 0; i < N; i++) {
 	    printf(" %d", A[j][i]);
@@ -35,21 +36,57 @@ int main()
     printf("\n");
     printf("\n");
 
-    for(int j = 0; j < N; j++) { // loop indexes only the bottom triangle of matrix and performs the transpose
 
-	for(int i = 0; i < N; i++) {
-
-	    if(i == j) {
-		break;
-	    } else {
+#pragma omp parallel sections
+{
+	clock_t begin2 = clock();
+	#pragma omp section  
+   {
+   		int j =0;
+	for(int i=j; i<N;i++)
+	{
+		if(i!=j)
+		{
 		int temp = A[j][i];
 		A[j][i] = A[i][j];
 		A[i][j] = temp;
 		temp = 0;
-	    }
+		}
 	}
-    }
+	} 
+	#pragma omp section  
+   {		int j =1;
+	for(int i=j; i<N;i++)
+	{
 
+		if(i!=j)
+		{
+		int temp = A[j][i];
+		A[j][i] = A[i][j];
+		A[i][j] = temp;
+		temp = 0;
+		}
+	}
+	}
+		#pragma omp section  
+   {		
+   int j =2;
+	for(int i=j; i<N;i++)
+	{
+
+		if(i!=j)
+		{
+		int temp = A[j][i];
+		A[j][i] = A[i][j];
+		A[i][j] = temp;
+		temp = 0;
+		}
+	}
+	} 
+	 	clock_t end2 = clock();
+}
+
+ printf("Transposed Array \n");
     for(int j = 0; j < N; j++) { // Prints out the transposed array now
 
 	for(int i = 0; i < N; i++) {
@@ -57,16 +94,21 @@ int main()
 	}
 	printf("\n");
     }
+    printf("\n");
+
+
+
 
     for(int i = 0; i < N; i++) { // frees all the memory in teh matrix A
 	free(A[i]);
     }
-
+	
     free(A);
 
-    clock_t end = clock();
-    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("%lf \n",time_spent);
+    //clock_t end = clock();
+	double time_spent1 = (double)(end1 - begin1) / CLOCKS_PER_SEC;
+    double time_spent2 = (double)(end2 - begin2) / CLOCKS_PER_SEC;
+    printf("%lf \n",time_spent1+ time_spent2);
  
 
     return 0;
