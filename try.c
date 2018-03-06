bbe168828096e325e69ece059e7605618257cdb1
Unknown <eardsa@gmail.com>
2018-03-06 16:01:54 +0000

    Pthread solution

---
+++ try.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+#include "omp.h"
+
+#define NTHREADS 4
+#define N 10
+int* A[N];
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+
+void *CalculateSum(void *args)
+{
+    int *argPtr = args;
+
+    int i,j,sum = 0;
+    int threadindex = *argPtr;
+
+    for(int i = 0; i < N; i++)
+    {
+	if (i % NTHREADS != threadindex) continue;
+	for(int j = 0; j < i; j++) 
+	{
+		int temp = A[i][j];
+		A[i][j] = A[j][i];
+		A[j][i] = temp;
+	}
+    }
+
+    pthread_mutex_lock( &mutex1 ); //Mutex must go here
+    pthread_mutex_unlock( &mutex1 );
+    // Pass the value back:
+    *argPtr = sum;
+}
+
+
+int main()
+{
+        //int N = 10;
+    pthread_t thread_id[NTHREADS];
+    int thread_args[NTHREADS];
+
+	clock_t begin1 = clock();
+	// Fill up all the indices of A
+	for (int j = 0; j < N; j++)
+	{
+		A[j] = malloc(N*sizeof(*A[j]));
+		for (int i = 0; i < N; i++)
+		{
+			//initialize values to 0
+			A[j][i] = (N * i) + j;
+		}
+	}    
+	clock_t end1 = clock();	 
+
+    
+    for(int j = 0; j < N; j++) // Prints the original array
+    { 
+		for(int i = 0; i < N; i++) 
+		{
+			printf(" %d", A[j][i]);
+		}
+	printf("\n");
+    }
+
+    printf("\n \n \n"); // Some new lines for the output
+ 
+    clock_t begin2 = clock();
+    
+    	pthread_mutex_init(&mutex1, NULL);
+	for(int i=0; i < NTHREADS; i++)
+	{
+		thread_args[i] = i;
+		pthread_create( &thread_id[i], NULL, CalculateSum, &thread_args[i]);
+	}
+//////////////////
+    
+    clock_t end2 = clock();
+    
+    for(int j = 0; j < N; j++)  // Prints out the transposed array now
+    {
+		for(int i = 0; i < N; i++) 
+		{
+			printf(" %d", A[j][i]);
+		}
+	printf("\n");
+    }
+
+    for(int i = 0; i < N; i++) { // frees all the memory in the matrix A
+	free(A[i]);
+    }
+    
+    double time_spent1 = (double)(end1 - begin1) / CLOCKS_PER_SEC;
+    double time_spent2 = (double)(end2 - begin2) / CLOCKS_PER_SEC;
+    printf("%lf \n",time_spent1+ time_spent2);
+ 
+    return 0;
+}
